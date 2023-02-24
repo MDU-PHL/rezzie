b099d03d50d45bae33b4afa69de10dd02291a217
Anders Goncalves da Silva <andersgs@gmail.com>
2023-02-24 08:41:26 -0800

    Add code to work with python versions 3.6 to 3.8

--- src/rezzie/__init__.py
+++ src/rezzie/__init__.py
@@ -9,19 +9,34 @@ __version__ = "0.1.0"
 
 # public API
 
-import atexit
-import importlib.resources
-from contextlib import ExitStack
+import sys
+import pathlib
 
+if sys.version_info < (3, 9):
+    from importlib_resources import files
 
-def get_path(module, *path):
-    """
-    Return a path to a resource shipped with your package. The path will last
-    for the duration of test execution, and will be cleaned up automatically.
-    This requires Python 3.9 or later.
-    """
-    file_manager = ExitStack()
-    atexit.register(file_manager.close)
-    ref = importlib.resources.files(module).joinpath(*path)
-    path = file_manager.enter_context(importlib.resources.as_file(ref))
-    return path
+    def get_path(module: str, *path: str) -> pathlib.Path:
+        """
+        Return a path to a resource shipped with your package. The path will last
+        for the duration of test execution, and will be cleaned up automatically.
+        This code works with versions of python 3.6 to 3.8.
+        """
+        path = files(module).joinpath(*path)
+        return path
+
+else:
+    import atexit
+    import importlib.resources
+    from contextlib import ExitStack
+
+    def get_path(module: str, *path: setattr) -> pathlib.Path:
+        """
+        Return a path to a resource shipped with your package. The path will last
+        for the duration of test execution, and will be cleaned up automatically.
+        This requires Python 3.9 or later.
+        """
+        file_manager = ExitStack()
+        atexit.register(file_manager.close)
+        ref = importlib.resources.files(module).joinpath(*path)
+        path = file_manager.enter_context(importlib.resources.as_file(ref))
+        return path
